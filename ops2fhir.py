02e5d87fb1b1a71d862383735bfdd57c095de6a9
Andrea <andrea.essenwanger@bihealth.de>
2020-03-02 15:52:14 +0100

    Added Patient reference to MedicationStatement. MedicationStatement can be validated and posted.

--- ops2fhir.py
+++ ops2fhir.py
@@ -23,6 +23,8 @@ from fhirclient.models import (
     extension
 )
 
+import patientgenerator
+
 # TODO: function to create medication and validate it against server.
 def create_medication(col, meta_profile):
     pat_medication = medication.Medication()
@@ -38,10 +40,9 @@ def create_medication(col, meta_profile):
 def create_medication_statement(col, meta_profile, medication_id, patient_id):
     medication_statement = medicationstatement.MedicationStatement()
     medication_statement.meta = meta_profile
-    medication_statement.id = create_unique_id()
+    #medication_statement.id = create_unique_id()
     medication_statement.status = 'completed'
     # reference to medication object
-    # TODO: The corresponding Medication instance should already be on the server! Get ID from respective Medication object
     reference_medication = fhirreference.FHIRReference()
     reference_medication.reference = 'Medication/' + str(medication_id)
     medication_statement.medicationReference = reference_medication
@@ -69,7 +70,7 @@ def create_dosage(col):
     route_code = codeableconcept.CodeableConcept()
     route_code_coding = coding.Coding()
     route_code_coding.system = 'http://standardterms.edqm.eu'
-    route_code_coding.code = col['Routes and Methods of Administration - Concept Code']
+    route_code_coding.code = str(col['Routes and Methods of Administration - Concept Code'])
     route_code_coding.display = col['Routes and Methods of Administration - Term']
     route_code.coding = [route_code_coding]
     med_statement_dosage.route = route_code
@@ -157,7 +158,7 @@ def create_ingredient(col):
 
     return [med_ingredient]
 
-# Helper function for create_ingredient()
+# Helper function for create_ingredient_code_x()
 def create_ingredient_code_x(system, code, ingredient_ext):
     ingredient_code_coding = coding.Coding()
     ingredient_code_coding.system = system
@@ -167,7 +168,7 @@ def create_ingredient_code_x(system, code, ingredient_ext):
 
     return ingredient_code_coding
 
-# TODO: function shall make sure that the created UUIDs are unique (not yet in the server)
+# function no longer needed. Objects shall take ID given by the server...
 def create_unique_id():
     new_uuid = str(uuid.uuid4())
     # TODO: get all IDs from server and compare to new ID
@@ -175,7 +176,7 @@ def create_unique_id():
     return new_uuid
 
 # TODO: function to start session with vonk client
-def vonk_client(server_url, headers, verification):
+def vonk_client(headers, verification):
     vonk_session = requests.session()
     # TODO: Proxy parameter should be optional!!
     vonk_session.verify = verification
@@ -243,7 +244,7 @@ if __name__ == '__main__':
         'Content-Type': 'application/fhir+json; fhirVersion=4.0'
     }
 
-    vonk_session = vonk_client(server_url, headers, False)
+    vonk_session = vonk_client(headers, False)
 
     # Define profiles
     meta_medication = meta.Meta()
@@ -259,13 +260,45 @@ if __name__ == '__main__':
         # validate against profile
         validate_medication = vonk_session.post(f'{server_url}/Medication/$validate', data=json.dumps(pat_medication.as_json()))
         # if valid with profile, post to server
-        if validate_medication.status_code == 200:
-            post_medication = vonk_session.post(f'{server_url}/Medication/', data=json.dumps(pat_medication.as_json()))
-            # TODO: reference fhir_patient_generator
-            pat_id = '0000'
-            if post_medication.status_code == 201:
-                posted_medication = json.loads(post_medication.text)
-                med_id = posted_medication['id']
-                pat_med_statement = create_medication_statement(col, meta_medication_statement, med_id, pat_id)
-                validate_med_statement = vonk_session.post(f'{server_url}/MedicationStatement/$validate',
-                                                        data=json.dumps(pat_med_statement.as_json()))
+        if validate_medication.status_code != 200:
+            raise Exception(f'Failed to validate Medication resource: {validate_medication.text}')
+
+        post_medication = vonk_session.post(f'{server_url}/Medication/', data=json.dumps(pat_medication.as_json()))
+
+        if post_medication.status_code != 201:
+            raise Exception('Failed to create Medication')
+
+        posted_medication = json.loads(post_medication.text)
+        med_id = posted_medication['id']
+        print(f'Posted Medication: {med_id}')
+
+        post_patient = patientgenerator.generate_and_post_patients(
+            base_url=server_url,
+            profile_url='https://www.medizininformatik-initiative.de/fhir/core/StructureDefinition/Patient',
+            num_pat=1,
+            first_names_path='../fhir_patient_generator/input/first_names.json',
+            last_names_path='../fhir_patient_generator/input/last_names.json',
+            genders_path='../fhir_patient_generator/input/genders.json',
+            postal_codes_path='../fhir_patient_generator/input/zuordnung_plz_ort_landkreis.csv',
+            name_use='official',
+            ident_system='http://fhir.de/NamingSystem/gkv/kvid-10'
+        )
+        posted_patient = json.loads(post_patient.text)
+        pat_id = posted_patient['id']
+
+        pat_med_statement = create_medication_statement(col, meta_medication_statement, med_id, pat_id)
+        validate_med_statement = vonk_session.post(f'{server_url}/MedicationStatement/$validate',
+                                                   data=json.dumps(pat_med_statement.as_json()))
+
+        if validate_med_statement.status_code != 200:
+            raise Exception('Failed to validate MedicationStatement!')
+
+        post_med_statement = vonk_session.post(f'{server_url}/MedicationStatement',
+                                                   data=json.dumps(pat_med_statement.as_json()))
+
+        if post_med_statement.status_code != 201:
+            raise Exception('Failed to create MedicationStatement!')
+
+        posted_med_statement = json.loads(post_med_statement.text)
+        med_statement_id = posted_med_statement['id']
+        print(f'Posted MedicationStatement: {med_statement_id}')
