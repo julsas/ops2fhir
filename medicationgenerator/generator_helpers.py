7d3037a7222aebae562d64b0078341e1882b85cb
Andrea <andrea.essenwanger@bihealth.de>
2020-04-03 12:17:48 +0200

    Added usecols parameter to Pandas.read_csv usage to only read relevant cols from input csv. Deleted commented lines.

--- medicationgenerator/generator_helpers.py
+++ medicationgenerator/generator_helpers.py
@@ -2,9 +2,8 @@ import pandas as pd
 from typing import List
 
 class OpsCsvReader:
-    def __init__(self, file_path:str, encoding:str):
-        #self.data = OpsCsvReader.comma_to_dot(pd.read_csv(file_path, encoding=encoding), cols)
-        self.data = pd.read_csv(file_path, encoding=encoding)
+    def __init__(self, file_path:str, encoding:str, usecols):
+        self.data = pd.read_csv(file_path, encoding=encoding, usecols=usecols)
         self.path = file_path
         self.encoding = encoding
 
@@ -12,9 +11,7 @@ class OpsCsvReader:
     def comma_to_dot(self, col_names:List[str]):
         for col in col_names:
             self.data[col] = self.data[col].str.replace(',', '.').astype(float)
-            self.data[col] = (float(x.replace(',', '.')) for x in self.data[col])
 
-        #return self.data
 
 
     def as_str(self, col_names:List[str]):
