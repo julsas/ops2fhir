4f29a78bb7625d17ae1b7fe1dae7606705f05080
Essenwanger <andrea.essenwanger@bihealth.de>
2020-05-19 12:42:03 +0200

    Added procedure reference

--- examples/generator_usage.py
+++ examples/generator_usage.py
@@ -25,6 +25,7 @@ if __name__ == '__main__':
     ops_text_col = 'opsText'
     unit_code_col = 'UCUM-Code'
     unit_col = 'UCUM-Description'
+    ops_code_col = 'opsCode'
 
     csv_cols = [
         coding_display_col,
@@ -33,6 +34,7 @@ if __name__ == '__main__':
         ops_text_col,
         unit_code_col,
         unit_col,
+        ops_code_col
     ]
     csv_cols += numerical_cols
     csv_cols += coding_col_names
@@ -80,7 +82,15 @@ if __name__ == '__main__':
         unit_code_col=unit_code_col,
         unit_col=unit_col,
         unit_system='http://unitsofmeasure.org',
-        high_val_col=high_val_col
+        high_val_col=high_val_col,
+        procedure_profile='https://www.medizininformatik-initiative.de/fhir/core/StructureDefinition/Procedure',
+        procedure_status='completed',
+        procedure_category_system='http://snomed.info/sct',
+        procedure_category_code='182832007',
+        procedure_category_display='Procedure related to management of drug administration (procedure)',
+        procedure_ops_system='http://fhir.de/CodeSystem/dimdi/ops',
+        procedure_ops_code=ops_code_col,
+        procedure_ops_version='2020'
     )
 
     pathlib.Path('../output/posted_med_statements.txt').write_text(os.linesep.join(med_statement_ids))
