53bfb5922cd42d2bf6feb8993f40fde04b03d268
Essenwanger <de.andrea.essenwanger@gmail.com>
2020-07-23 15:50:18 +0200

    Added client.py classes

--- medicationgenerator/__init__.py
+++ medicationgenerator/__init__.py
@@ -1,2 +1,3 @@
 from medicationgenerator.generate import generate_and_post_medications, generate_and_post, generate_and_post_procedure
 from medicationgenerator.generator_helpers import OpsCsvReader
+from medicationgenerator.client import FhirClient, ResourceEnum
